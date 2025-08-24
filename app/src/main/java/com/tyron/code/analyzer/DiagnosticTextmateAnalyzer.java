b3c481419ffa0d50a4881d47a4efd524b6d314cc
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-08-24 19:26:45 +0200

    Update DiagnosticTextmateAnalyzer.java

--- app/src/main/java/com/tyron/code/analyzer/DiagnosticTextmateAnalyzer.java
+++ app/src/main/java/com/tyron/code/analyzer/DiagnosticTextmateAnalyzer.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import io.github.rosemoe.sora.lang.brackets.BracketsProvider;
+import io.github.rosemoe.sora.lang.diagnostic.DiagnosticsContainer;
 
 public abstract class DiagnosticTextmateAnalyzer extends BaseTextmateAnalyzer {
 
