faf0309c7d5c7083907685943d7a308a66084abf
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-09-03 13:34:15 +0200

    Update SemanticAnalyzeManager.java

--- app/src/main/java/com/tyron/code/analyzer/SemanticAnalyzeManager.java
+++ app/src/main/java/com/tyron/code/analyzer/SemanticAnalyzeManager.java
@@ -23,12 +23,11 @@ public abstract class SemanticAnalyzeManager extends DiagnosticTextmateAnalyzer 
   public SemanticAnalyzeManager(
       Editor editor,
       String grammarName,
-      String scopeName,
       InputStream grammarIns,
       Reader languageConfiguration,
       IRawTheme theme)
       throws Exception {
-    super(editor, grammarName,scopeName, grammarIns, languageConfiguration, theme);
+    super(editor, grammarName, grammarIns, languageConfiguration, theme);
   }
 
   public abstract List<SemanticToken> analyzeSpansAsync(CharSequence contents);
