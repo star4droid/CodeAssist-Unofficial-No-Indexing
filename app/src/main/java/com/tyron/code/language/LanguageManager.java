d9c2a220ebe5efd7d2b48eacb3593f44ee200311
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-09-20 10:32:40 +0200

    Update LanguageManager.java

--- app/src/main/java/com/tyron/code/language/LanguageManager.java
+++ app/src/main/java/com/tyron/code/language/LanguageManager.java
@@ -1,15 +1,28 @@
 package com.tyron.code.language;
 
+import android.content.res.AssetManager;
+
+import com.tyron.code.ApplicationLoader;
 import com.tyron.code.language.groovy.Groovy;
 import com.tyron.code.language.java.Java;
 import com.tyron.code.language.json.Json;
 import com.tyron.code.language.kotlin.Kotlin;
 import com.tyron.code.language.xml.Xml;
 import com.tyron.editor.Editor;
+
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Supplier;
+
+import io.github.rosemoe.sora.langs.textmate.TextMateColorScheme;
+import io.github.rosemoe.sora.langs.textmate.TextMateLanguage;
+import io.github.rosemoe.sora.widget.CodeEditor;
+
 import io.github.rosemoe.sora.langs.textmate.TextMateLanguage;
 import io.github.rosemoe.sora.langs.textmate.registry.GrammarRegistry;
 import io.github.rosemoe.sora.langs.textmate.registry.ThemeRegistry;
@@ -17,55 +30,61 @@ import io.github.rosemoe.sora.langs.textmate.registry.provider.AssetsFileResolve
 import org.eclipse.tm4e.core.registry.IGrammarSource;
 import org.eclipse.tm4e.languageconfiguration.internal.model.LanguageConfiguration;
 import java.nio.charset.StandardCharsets;
-
+import io.github.rosemoe.sora.lang.EmptyLanguage;
 
 public class LanguageManager {
 
-  private static LanguageManager Instance = null;
+    private static LanguageManager Instance = null;
 
-  public static LanguageManager getInstance() {
-    if (Instance == null) {
-      Instance = new LanguageManager();
+    public static LanguageManager getInstance() {
+        if (Instance == null) {
+            Instance = new LanguageManager();
+        }
+        return Instance;
     }
-    return Instance;
-  }
 
-  private final Set<Language> mLanguages = new HashSet<>();
+    private final Set<Language> mLanguages = new HashSet<>();
 
-  private LanguageManager() {
-    initLanguages();
-  }
-
-  private void initLanguages() {
-    mLanguages.addAll(Arrays.asList(new Xml(), new Java(), new Kotlin(), new Groovy(), new Json()));
-  }
-
-  public boolean supports(File file) {
-    for (Language language : mLanguages) {
-      if (language.isApplicable(file)) {
-        return true;
-      }
+    private LanguageManager() {
+        initLanguages();
     }
-    return false;
-  }
 
-  public io.github.rosemoe.sora.lang.Language get(Editor editor, File file) {
-    for (Language lang : mLanguages) {
-      if (lang.isApplicable(file)) {
-        return lang.get(editor);
-      }
+    private void initLanguages() {
+        mLanguages.addAll(
+                Arrays.asList(
+                        new Xml(),
+                        new Java(),
+                        new Kotlin(),
+                        new Groovy(),
+                        new Json()));
     }
-    return null;
-  }
 
-  public static TextMateLanguage createTextMateLanguage(
+    public boolean supports(File file) {
+        for (Language language : mLanguages) {
+            if (language.isApplicable(file)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public io.github.rosemoe.sora.lang.Language get(Editor editor, File file) {
+        for (Language lang : mLanguages) {
+            if (lang.isApplicable(file)) {
+                return lang.get(editor);
+            }
+        }
+        return null;
+    }
+
+    public static TextMateLanguage createTextMateLanguage(
         String scopeName,
         String grammarAssetPath,
         String configAssetPath,
-        Editor editor) throws IOException {
+        Editor editor) {
 
     AssetManager assets = ApplicationLoader.getInstance().getAssets();
-
+    try{
     /* 1. obtain the registries */
     GrammarRegistry grammarReg = GrammarRegistry.getInstance();
     ThemeRegistry themeReg = ThemeRegistry.getInstance();
@@ -97,6 +116,9 @@ public class LanguageManager {
     //lang.setLanguageConfiguration(config);
 
     return lang;
+    } catch(IOException e) {
+      return new EmptyLanguage();
+    }
 }
 
 }
