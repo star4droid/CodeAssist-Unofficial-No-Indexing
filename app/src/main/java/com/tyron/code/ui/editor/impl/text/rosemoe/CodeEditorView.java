4f26e69dd6677922f0acdfd991e1ea22ef97cff5
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-09-20 12:07:56 +0200

    Update CodeEditorView.java

--- app/src/main/java/com/tyron/code/ui/editor/impl/text/rosemoe/CodeEditorView.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/text/rosemoe/CodeEditorView.java
@@ -393,11 +393,12 @@ public class CodeEditorView extends CodeEditor implements Editor {
     return CodeEditorView.super.formatCodeAsync();
   }
 
- /* @Override
+ @Override
   public synchronized boolean formatCodeAsync(int start, int end) {
-    if (isFormatting()) {
+   /* if (isFormatting()) {
       return false;
-    }
+    }*/
+    if(true) return false;
     if (getEditorLanguage() instanceof EditorFormatter
         && getText() != null
         && getEditorLanguage() != null) {
@@ -422,7 +423,7 @@ public class CodeEditorView extends CodeEditor implements Editor {
       return true;
     }
     return false;
-  }*/
+  }
 
   @Override
   public Caret getCaret() {
