e32d878940098b353b036947276e5fd29d9e0b71
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2025-09-23 15:35:25 +0200

    Update EmptyTextMateLanguage.java

--- app/src/main/java/com/tyron/code/language/textmate/EmptyTextMateLanguage.java
+++ app/src/main/java/com/tyron/code/language/textmate/EmptyTextMateLanguage.java
@@ -1,7 +1,82 @@
 package com.tyron.code.language.textmate;
 
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import io.github.rosemoe.sora.lang.Language;
 import io.github.rosemoe.sora.lang.EmptyLanguage;
+import io.github.rosemoe.sora.lang.QuickQuoteHandler;
+import io.github.rosemoe.sora.lang.analysis.AnalyzeManager;
+import io.github.rosemoe.sora.lang.completion.CompletionPublisher;
+import io.github.rosemoe.sora.lang.smartEnter.NewlineHandler;
+import io.github.rosemoe.sora.lang.format.Formatter;
+import io.github.rosemoe.sora.text.CharPosition;
+import io.github.rosemoe.sora.text.ContentReference;
+import io.github.rosemoe.sora.widget.SymbolPairMatch;
 
-public class EmptyTextMateLanguage extends EmptyLanguage {
-   public boolean createIdentifiers = false;
+public class EmptyTextMateLanguage extends EmptyLanguage implements Language {
+  public boolean createIdentifiers = false;
+
+  @Override
+  public void destroy() {
+    super.destroy();
+  }
+
+  @Override
+  public io.github.rosemoe.sora.lang.analysis.AnalyzeManager getAnalyzeManager() {
+    return super.getAnalyzeManager();
+  }
+
+  @Override
+  public io.github.rosemoe.sora.lang.format.Formatter getFormatter() {
+    return super.getFormatter();
+  }
+
+  @Override
+  public int getIndentAdvance(
+      @NonNull ContentReference content,
+      int line,
+      int column,
+      int spaceCountOnLine,
+      int tabCountOnLine) {
+    return super.getIndentAdvance(content, line, column, spaceCountOnLine, tabCountOnLine);
+  }
+
+  @Override
+  public int getIndentAdvance(@NonNull ContentReference content, int line, int column) {
+    return super.getIndentAdvance(content, line, column);
+  }
+
+  @Override
+  public int getInterruptionLevel() {
+    return super.getInterruptionLevel();
+  }
+
+  @Override
+  public io.github.rosemoe.sora.lang.smartEnter.NewlineHandler[] getNewlineHandlers() {
+    return super.getNewlineHandlers();
+  }
+
+  @Override
+  public io.github.rosemoe.sora.lang.QuickQuoteHandler getQuickQuoteHandler() {
+    return super.getQuickQuoteHandler();
+  }
+
+  @Override
+  public io.github.rosemoe.sora.widget.SymbolPairMatch getSymbolPairs() {
+    return super.getSymbolPairs();
+  }
+
+  @Override
+  public void requireAutoComplete(
+      @NonNull ContentReference content,
+      @NonNull CharPosition position,
+      @NonNull CompletionPublisher publisher,
+      @NonNull Bundle extraArguments) {
+    super.requireAutoComplete(content, position, publisher, extraArguments);
+  }
+
+  @Override
+  public boolean useTab() {
+    return super.useTab();
+  }
 }
